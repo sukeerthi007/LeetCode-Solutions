bb0dc1d3216ee00ac2f16e9cf92456b58c936baa
sukeerthi007 <m.sukeerthi07@gmail.com>
2026-06-29 19:42:40 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (19.78%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.py
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.py
@@ -4,5 +4,5 @@ class Solution:
         for i in patterns :
             if i in word :
                 count += 1 
-                print("Found!")
+                #print("Found!")
         return count 
